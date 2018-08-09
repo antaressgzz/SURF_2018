6e721422b56e446ebcdf593bad5997c8c05317c0
antaressgzz <antaressgzz@gmail.com>
2018-08-09 09:59:40 +0800

    explanation

--- core/config.py
+++ core/config.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 
 network_config = {
     'cnn_fc':{
-        'type':'cnn_fc',
-        'cnn_layer': {'kernel':(3, 48), 'filter':(2, 10)},
+        'type':'cnn_fc',                                      #
+        'cnn_layer': {'kernel':(3, 48), 'filter':(2, 10)},    # kernal size, number of kernels
         'cnn_activation': tf.nn.relu,
-        'fc_layer': (1000, 1000),
+        'fc_layer': [1000, 1000],                             # 0 to 2 hidden layers, size 1000
         'fc_activation': tf.nn.tanh,
-        'additional_input': 'portfolio_value',
-        'output_num': None,
+        'additional_input': 'weights',                        # last_weights or None
+        'output_num': None,                                   #
         'output_activation': None,
         'weights_regularization': None,
         'bias_regularization': None
@@ -16,9 +16,9 @@ network_config = {
 
     'fc':{
         'type': 'fc',
-        'fc_layer': (1000, 1000), # 0 to 2 hidden layers
+        'fc_layer': [1000, 1000],                             # 0 to 2 hidden layers, size 1000
         'fc_activation': tf.nn.tanh,
-        'additional_input': 'portfolio_value', # portfolio_value, last_weights, None
+        'additional_input': 'weights',                        # last_weights or None
         'output_num': None,
         'output_activation': None,
         'weights_regularization': None,
