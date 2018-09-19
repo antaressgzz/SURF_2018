3676975061117440ea75db16e1b13a95630862df
YaqiLi-Katherine <37947571+YaqiLi-Katherine@users.noreply.github.com>
2018-09-19 10:15:45 +0100

    Update runthis.py

--- data/data_raw/runthis.py
+++ data/data_raw/runthis.py
@@ -4,7 +4,7 @@ import datetime
 # 375天的数据， 15天／ep，25个ep, 2017-07-01 to 2018-07-11
 startdate="2015-04-01"
 start = datetime.datetime.strptime(startdate, '%Y-%m-%d')
-episodes=150
+episodes=97
 days_per_ep=15
 for ep in range(episodes):
     delta=datetime.timedelta(days=ep*15)
@@ -14,8 +14,10 @@ for ep in range(episodes):
     deltatwo=datetime.timedelta(days=(ep+1)*15)
     enddate=start+deltatwo
     end=enddate.strftime('%Y-%m-%d')
+    instrument="USD_CAD"
+    #USD_JPY EUR_USD GBP_USD USD_CAD
 
-    DataColr = test.test(new, end, "USD_JPY")
+    DataColr = test.test(new, end,instrument ,"M30",str('csvDocu/30M/'+instrument+'_M30.db'))
 
 
 
