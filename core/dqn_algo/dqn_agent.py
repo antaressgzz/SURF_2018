9c826203039faf887da10848a4ec4759c7ffa248
antaressgzz <antaressgzz@gmail.com>
2018-10-02 10:55:25 +0100

    deeper

--- core/dqn_algo/dqn_agent.py
+++ core/dqn_algo/dqn_agent.py
@@ -117,61 +117,41 @@ class Dqn_agent:
                                  kernel_regularizer=regularizer, bias_regularizer=regularizer,
                                  kernel_initializer=w_initializer, bias_initializer=b_initializer,
                                  padding="VALID", name='conv1')
-        print(conv1.shape)
+        print('conv1:', conv1.shape)
+
         conv2 = tf.layers.conv2d(conv1, filters=filters[1], kernel_size=kernels[1], strides=strides[1], trainable=True,
                                  use_bias=cnn_bias, activation=activation,
                                  kernel_regularizer=regularizer, bias_regularizer=regularizer,
                                  kernel_initializer=w_initializer, bias_initializer=b_initializer,
                                  padding="VALID", name='conv2')
-        print(conv2.shape)
-        if weights_pos is None:
-            fc_input = tf.layers.flatten(conv2)
-            print(fc_input.shape)
-            fc1 = layers.fully_connected(fc_input, num_outputs=fc_size, activation_fn=activation,
-                                         weights_regularizer=regularizer,
-                                         weights_initializer=w_initializer,
-                                         biases_regularizer=regularizer,
-                                         biases_initializer=b_initializer,
-                                         trainable=True, scope='fc1')
-            print(fc1.shape)
-            output = layers.fully_connected(fc1, num_outputs=self.action_num, activation_fn=None,
-                                            weights_regularizer=regularizer,
-                                            biases_regularizer=regularizer,
-                                            weights_initializer=w_initializer,
-                                            trainable=True, scope='output')
-        elif weights_pos == 0:
-            fc_input = tf.concat([tf.layers.flatten(conv2), addi_input], axis=1)
-            print(fc_input.shape)
-            fc1 = layers.fully_connected(fc_input, num_outputs=fc_size, activation_fn=activation,
-                                         weights_regularizer=regularizer,
-                                         weights_initializer=w_initializer,
-                                         biases_regularizer=regularizer,
-                                         biases_initializer=b_initializer,
-                                         trainable=True, scope='fc1')
-            output = layers.fully_connected(fc1, num_outputs=self.action_num, activation_fn=None,
-                                            weights_regularizer=regularizer,
-                                            biases_regularizer=regularizer,
-                                            weights_initializer=w_initializer,
-                                            trainable=True, scope='output')
-        elif weights_pos == 1:
-            fc_input = tf.layers.flatten(conv2)
-            fc1 = layers.fully_connected(fc_input, num_outputs=fc_size, activation_fn=activation,
-                                         weights_regularizer=regularizer,
-                                         weights_initializer=w_initializer,
-                                         biases_regularizer=regularizer,
-                                         biases_initializer=b_initializer,
-                                         trainable=True, scope='fc1')
-            fc1_ = tf.concat([fc1, addi_input], axis=1)
-            print(fc1_.shape)
-            output = layers.fully_connected(fc1_, num_outputs=self.action_num, activation_fn=None,
-                                            weights_regularizer=regularizer,
-                                            biases_regularizer=regularizer,
-                                            weights_initializer=w_initializer,
-                                            trainable=True, scope='output')
-        else:
-            fc_input = None
-            output = None
-            print('Invalid weights_pos.')
+        print('conv2:', conv2.shape)
+
+        conv3 = tf.layers.conv2d(conv2, filters=filters[2], kernel_size=kernels[2], strides=strides[2], trainable=True,
+                                 use_bias=cnn_bias, activation=activation,
+                                 kernel_regularizer=regularizer, bias_regularizer=regularizer,
+                                 kernel_initializer=w_initializer, bias_initializer=b_initializer,
+                                 padding="VALID", name='conv3')
+        print('conv3:', conv3.shape)
+
+        fc_input = tf.layers.flatten(conv3)
+        print('fc_input:', fc_input.shape)
+        fc1 = layers.fully_connected(fc_input, num_outputs=fc_size, activation_fn=activation,
+                                     weights_regularizer=regularizer,
+                                     weights_initializer=w_initializer,
+                                     biases_regularizer=regularizer,
+                                     biases_initializer=b_initializer,
+                                     trainable=True, scope='fc1')
+
+
+        print('fc1:', fc1.shape)
+        output = layers.fully_connected(fc1, num_outputs=self.action_num, activation_fn=None,
+                                        weights_regularizer=regularizer,
+                                        biases_regularizer=regularizer,
+                                        weights_initializer=w_initializer,
+                                        trainable=True, scope='output')
+
+
+
 
         return fc_input, output
 
