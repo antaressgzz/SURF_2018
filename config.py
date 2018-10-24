02ee6fdd98d90ca391a70e0b3da905656b2a6dcf
antaressgzz <antaressgzz@gmail.com>
2018-10-24 19:05:50 +0100

    train single cost model

--- config.py
+++ config.py
@@ -1,37 +1,47 @@
-FEE = False
+FEE = True
 EXP_KEY = 2300
 MAX_EVALS = 1
 number_workers = 1
-mode = 'auto'
-name = '2300'
+mode = 'single'
 
-config_fee = {
-        'freeze_cnn': False,
-        'kernels':[[1, 3], [1, 3]],
-        'strides':[[1, 1], [1, 1]],
-        'filters':[3, 3],
-        'fc1_size':64,
-        'fc2_size':64,
-        'padding': 'same',
-        'regularizer': 0.000697715451566933,
-        'b_initializer': 0,
-        'w_initializer': 0.01,
-        'cnn_activation': 'selu',
-        'fc_activation': 'selu'
-    }
 
-config_nofee = {
-        'kernels':[[1, 3], [1, 3]],
-        'strides':[[1, 1], [1, 1]],
-        'filters':[3, 3],
-        'padding': 'same',
-        'regularizer': 0.000697715451566933,
-        'fc_size': 32,
-        'b_initializer': 0,
-        'w_initializer': 0.01,
-        'cnn_activation': 'selu',
-        'fc_activation': 'selu'
-    }
+tuned_config = {"env": {"window_length": 200,
+                         "input": "rf",
+                         "norm": "previous",
+                         "talib": False,
+                         ################## change this ###############
+                         "trading_cost": 0.0},
+     "train": {"learning_rate": 0.00015309751147495794,
+               "division": 6,
+               "epsilon": 1,
+               "reward_scale": 1200.0,
+               "batch_size": 32,
+               "replay_period": 8,
+               "memory_size": 20000,
+               "dropout": 0.4893817909286,
+               ############### change these #################
+               "upd_tar_prd": 2000,
+               "steps": 200000,
+               "save": True,
+               "save_period": 40000,
+               "GPU": True,
+               "discount": 0},
+     "net": {"kernels": [[1, 10], [4, 4]],
+             "strides": [[1, 1], [1, 3]],
+             "filters": [4, 4],
+             "padding": "same",
+             "regularizer": 0.003781580642373421,
+             "fc1_size": 64,
+             "b_initializer": 0,
+             "w_initializer": 0.01,
+             "cnn_activation": "relu",
+             "fc_activation": "leaky_relu",
+             "output_num": 210,
+             #################### change this ###################
+             "fc2_size": 64,
+             "process_cost": True,
+             "freeze_cnn": False
+             }}
 
 def get_config(fee):
         config = {}
@@ -63,69 +73,35 @@ def get_config(fee):
                 ##########################
         }
         if fee is True:
-                config['net'] = config_fee
+                config['net'] =     {
+                                        'freeze_cnn': False,
+                                        'kernels':[[1, 3], [1, 3]],
+                                        'strides':[[1, 1], [1, 1]],
+                                        'filters':[3, 3],
+                                        'fc1_size':64,
+                                        'fc2_size':64,
+                                        'padding': 'same',
+                                        'regularizer': 0.000697715451566933,
+                                        'b_initializer': 0,
+                                        'w_initializer': 0.01,
+                                        'cnn_activation': 'selu',
+                                        'fc_activation': 'selu'
+                                    }
                 config['env']['trading_cost'] = 0.0001
                 config['train']['discount'] = 0.9
         else:
-                config['net'] = config_nofee
+                config['net'] =    {
+                                        'kernels':[[1, 3], [1, 3]],
+                                        'strides':[[1, 1], [1, 1]],
+                                        'filters':[3, 3],
+                                        'padding': 'same',
+                                        'regularizer': 0.000697715451566933,
+                                        'fc_size': 32,
+                                        'b_initializer': 0,
+                                        'w_initializer': 0.01,
+                                        'cnn_activation': 'selu',
+                                        'fc_activation': 'selu'
+                                    }
                 config['env']['trading_cost'] = 0
                 config['train']['discount'] = 0
         return config
-
-param_space = {
-    # train
-    'steps': 240000,
-    'learning_rate': 0.0001270642752036939,
-    'reward_scale': 1200,
-    # 'discount': hp.uniform('discount', 0, 1),
-    'epsilon': 0.1,
-    'batch_size': 128,
-    'replay_period': 8,
-    'division': 3,
-    'dropout': 0.5646002767503104,
-    # net
-    'activation': 'selu',
-    'fc_size': 64,
-    'kernels': [[1, 10],
-                [4, 5]],
-    'filters': [8, 2],
-    'strides': [[1, 2], [1, 3]],
-    'regularizer': 1.0081378980963206e-05,
-    'cnn_activation': 'selu',
-    'fc_activation': 'selu',
-    # env
-    'padding': 'valid',
-    'window_length': 250,
-    'input': 'rf',
-    'norm': 'latest_close',
-}
-
-param_space_fee = {
-    # train
-    'steps': 240000,
-    'learning_rate': 0.0001270642752036939,
-    'reward_scale': 1200,
-    'process_cost': False,
-    'discount': 0.9,
-    'epsilon': 1,
-    'batch_size': 128,
-    'replay_period': 8,
-    'division': 3,
-    'dropout': 0.5646002767503104,
-    # net
-    'freezn_cnn': False,
-    'fc1_size': 64,
-    'fc2_size': 64,
-    'kernels': [[1, 10],
-                [4, 5]],
-    'filters': [8, 2],
-    'strides': [[1, 2], [1, 3]],
-    'regularizer': 1.0081378980963206e-05,
-    'cnn_activation': 'selu',
-    'fc_activation': 'selu',
-    # env
-    'padding': 'valid',
-    'window_length': 250,
-    'input': 'rf',
-    'norm': 'latest_close',
-}
--- main.py
+++ main.py
@@ -4,22 +4,16 @@ current_path = os.getcwd()
 sys.path.append(current_path)
 from hypertune import start_commander, start_workers
 from coordinator import Coordinator
-from config import get_config, param_space_fee, param_space, FEE, EXP_KEY, mode, name, number_workers
+from config import mode, number_workers, tuned_config
 from hypertune import construct_config
 # %matplotlib inline
 
 
-if mode == 'auto':
+if mode == 'parallel':
     start_commander()
     workers = start_workers(number_workers)
 else:
-    config = get_config(FEE)
-    if FEE:
-        params = param_space_fee
-    else:
-        params = param_space
-    config = construct_config(config, params)
-    model = Coordinator(config, name)
+    model = Coordinator(tuned_config, '-5.28_fee')
     # model.restore('2102-160000')
     model.train('single', True)
     model.back_test('test', 12000, True)
